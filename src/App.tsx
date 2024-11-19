1a4295a371227f4b078dd01de1f5a9c7ae91ae1e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-19 16:10:02 +0100

    put the content in the center of the webpage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import AccountDetails from "./component/AccountDetails/accountDetails";
+import AllChat from "./component/AllChats/allChat";
+import SideBar from "./component/sideBar";
 
 function App() {
   return (
     <>
-      <div className="w-full h-full border-2 border-red-500">
-       hello this is you chat application 
+      <div className="flex justify-center items-center w-full h-screen bg-slate-200">
+        <div className="flex max-w-4xl w-full h-[80vh] bg-slate-500 rounded-lg shadow-lg">
+          <SideBar />
+          <AllChat />
+        </div>
+        {/* <BrowserRouter>
+          <Routes>
+            <Route path="/allChats" element={<AllChat />} />
+            <Route path="/accountDetails" element={<AccountDetails />} />
+          </Routes>
+        </BrowserRouter> */}
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

d981951dab720e3fc0c7cd4fdc2cc62ea4989c79
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-19 11:13:34 +0100

    first intial project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-full border-2 border-red-500">
+       hello this is you chat application 
+      </div>
+    </>
+  )
+}
+
+export default App

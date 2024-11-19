3e120293ed736729b7c6316051410771c7e50f7e
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-19 16:10:27 +0100

    define icons for the allChats and logout

--- src/component/sideBar.tsx
+++ src/component/sideBar.tsx
@@ -1,9 +1,18 @@
-
+import { BiLogOutCircle } from "react-icons/bi";
+import { LuMessagesSquare } from "react-icons/lu";
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <div className="w-16 h-full bg-slate-600 justify-around items-center flex flex-col">
+      <button className="border-2 border-slate-400 rounded-lg p-2">
+        <LuMessagesSquare size={20} color="white" />
+      </button>
 
-export default SideBar
+      <button className=" border-2 border-slate-400 rounded-lg p-2">
+        <BiLogOutCircle size={20} color="white" />
+      </button>
+    </div>
+  );
+};
+
+export default SideBar;

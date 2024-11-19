2550e1ac2bac028bfd52284ef84f033d5088c250
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-19 18:53:23 +0100

    implement the chat user navbar

--- src/component/AllChats/chatConversation.tsx
+++ src/component/AllChats/chatConversation.tsx
@@ -1,8 +1,24 @@
+import { BsThreeDotsVertical } from "react-icons/bs"
+import { CiSearch } from "react-icons/ci"
+import { IoIosCall } from "react-icons/io"
 
 
 const ChatConversation = () => {
   return (
-    <div>ChatConversation</div>
+    <div className="w-full flex flex-col">
+      <div className="flex w-full h-20 bg-slate-800 border-b-2 border-slate-400 justify-center items-center px-2">
+        <div className="flex flex-col w-full h-fit">
+        <h1 className="font-bold text-2xl text-gray-300">User Name</h1>
+        <span className="text-xs text-blue-300">online 2h ago</span>
+        </div>
+        <div className="flex space-x-8">
+        <CiSearch size={25} color="white" className="cursor-pointer"/>
+        <IoIosCall size={25} color="white" className="cursor-pointer"/>
+        <BsThreeDotsVertical size={25} color="white" className="cursor-pointer"/>
+      </div>
+      </div>
+      
+    </div>
   )
 }
 

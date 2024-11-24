d13ff82f73ecb7bd9e86a8e4c657adbc4892b7a6
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-24 10:59:00 +0100

    implement the routing and the logic of authUser to protectedRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 // import AccountDetails from "./component/AccountDetails/accountDetails";
 import AllChat from "./component/AllChats/allChat";
-import SideBar from "./component/sideBar";
+import Authentication from "./component/auth/authentication";
+// import SideBar from "./component/sideBar";
+import {ProtectedRoutes} from "./component/protectedRoutes";
+
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "./firebase";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+        setIsLoading(false);
+        return;
+      }
+      setUser(null);
+      setIsLoading(false)
+    });
+
+    return () => unSubscribe();
+  });
+
+  if(isLoading) return <div>Loading...</div>
+
   return (
     <>
       <div className="flex justify-center items-center w-full h-screen bg-slate-200">
-        <div className="flex max-w-4xl w-full h-[80vh] bg-slate-500 shadow-lg">
-          <SideBar />
-          <AllChat />
+        <div className="flex max-w-4xl w-full h-[80vh] bg-slate-500 shadow-inner">
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Authentication />} />
+              <Route
+                path="/allChat"
+                element={
+                  <ProtectedRoutes user={user}>
+                    <AllChat />
+                  </ProtectedRoutes>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+          {/* <SideBar />
+           */}
         </div>
-        {/* <BrowserRouter>
-          <Routes>
-            <Route path="/allChats" element={<AllChat />} />
-            <Route path="/accountDetails" element={<AccountDetails />} />
-          </Routes>
-        </BrowserRouter> */}
       </div>
     </>
   );

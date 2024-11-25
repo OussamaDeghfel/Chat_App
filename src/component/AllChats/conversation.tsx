d42e18453eeed3192dd81347c498ebcfe3929083
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-11-25 14:57:46 +0100

    retrieve messages from the firestore collection

--- src/component/AllChats/conversation.tsx
+++ src/component/AllChats/conversation.tsx
@@ -1,13 +1,22 @@
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import {db} from '../../firebase'
+import { useState } from "react";
+
 const Conversation = () => {
+
+  const [text, setText] = useState<string[]>()
+
+  const messagesRef = collection(db, 'messages')
+  const q = query(messagesRef, orderBy('text', 'asc'))
+  onSnapshot(q, (snapshot) => {
+    const messages = snapshot.docs.map((doc) => doc.data().text)
+    setText(messages)
+  })
+
   return (
     <div className="p-2 justify-end items-end bottom-0 space-y-4 flex-reverse">
-      <div className="flex justify-end">
-        <p className="bg-blue-500 p-4 w-fit rounded-md">what are you doing ?</p>
-      </div>
-      <div className="flex">
-        <p className="bg-slate-300 p-4 w-fit rounded-md">
-          Nothing .. i want to go hiking
-        </p>
+      <div className="flex flex-col justify-end items-end">
+        <p className="bg-blue-500 p-4 w-fit rounded-md">{text}</p>
       </div>
     </div>
   );

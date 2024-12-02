f8a0eb361a7c807b48e6a6eb9cc246d476fd3aa8
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-12-02 12:07:27 +0100

    get the message detail to show the full information

--- src/component/AllChats/chatMessages.tsx
+++ src/component/AllChats/chatMessages.tsx
@@ -12,32 +12,36 @@ const ChatMessages = () => {
   const usersList = useSelector((state: RootState) => state.user.usersList);
   const dispatch = useDispatch()
 
-  console.log("userList from chatMessages: ", usersList.map((user) => user.userName));
-
   useEffect(() => {
     getUsers()(dispatch)
   },[dispatch])
 
-  const currentDate = new Date();
+  // usersList.map((user) => {
+  //   console.log(user.userId, user.message.text, user.message.sender);
+  // });
 
-  const calculedTime = (time: Date) => {
-    const timeDiff = currentDate.getTime() - time.getTime();
-    const minutes = Math.floor(timeDiff / 60000);
-    if (minutes < 1) {
-      return "now";
-    }
-    if (minutes < 60) {
-      return minutes + "m ago";
-    }
-    if (minutes >= 60 && minutes < 1440) {
-      const hours = Math.floor(minutes / 60);
-      return hours + "h ago";
-    }
-    if (minutes >= 1440) {
-      const days = Math.floor(minutes / 1440);
-      return days + "d ago";
-    }
-  };
+  // const currentDate = new Date();
+
+  // const calculedTime = (time: Date) => {
+  //   const timeDiff = currentDate.getTime() - time.getTime();
+  //   const minutes = Math.floor(timeDiff / 60000);
+  //   if (minutes < 1) {
+  //     return "now";
+  //   }
+  //   if (minutes < 60) {
+  //     return minutes + "m ago";
+  //   }
+  //   if (minutes >= 60 && minutes < 1440) {
+  //     const hours = Math.floor(minutes / 60);
+  //     return hours + "h ago";
+  //   }
+  //   if (minutes >= 1440) {
+  //     const days = Math.floor(minutes / 1440);
+  //     return days + "d ago";
+  //   }
+  // };
+
+  
   return (
     <div className="w-60 h-full bg-slate-800 p-2 border-r-2 border-slate-400 pt-8 flex flex-col">
       <Form>
@@ -54,31 +58,31 @@ const ChatMessages = () => {
           return (
             <div
               className={`w-full h-16 flex rounded-lg p-2 bg-slate-600 justify-center items-center space-x-1 cursor-pointer `}
-              key={user.id}
+              key={user.userId}
             >
-              {/* <div className="w-10 h-10 flex justify-center items-center">
-                <img
+              <div className="w-10 h-10 flex justify-center items-center">
+                {/* <img
                   src={user.image}
                   alt="user image"
                   className=" rounded-md"
-                />
-              </div> */}
+                /> */}
+              </div>
               <div className="w-full h-full flex flex-col space-y-1 justify-center items-start">
-                <h1 className="font-medium text-xl text-white">{user.userName}</h1>
-                {/* <p className="text-xs text-gray-300 overflow-hidden">
-                  {`${user.text
+                <h1 className="font-medium text-xl text-white">{user.message.sender}</h1>
+                <p className="text-xs text-gray-300 overflow-hidden">
+                  {`${user.message.text
                     .split("")
                     .slice(0, 20)
                     .join("")}`}
                   ...
-                </p> */}
+                </p>
               </div>
               <div className="w-10 h-10 flex flex-col gap-1 place-content-center">
                 <div className="w-full h-full flex justify-center items-center">
                   <IoCheckmarkDoneSharp size={15} className="hidden" />
-                  {/* <span className="text-xs text-gray-300">
-                    {calculedTime(user.timestamp)}
-                  </span> */}
+                  <span className="text-xs text-gray-300">
+                    {/* {user.message.timestamp.toDateStri} */}
+                  </span>
                 </div>
                 <div className="w-full h-full flex justify-center items-center">
                   <span className="bg-blue-500 rounded-full text-sm text-center w-full h-full hidden">
--- src/redux/usersSlice/actions.tsx
+++ src/redux/usersSlice/actions.tsx
@@ -1,43 +1,34 @@
-// export const fetchUsers = createAsyncThunk(
-//   "user/fetchUsers",
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const usersRef = collection(db, "messages");
-//       const q = query(usersRef);
-//       const querySnapshot = await getDocs(q);
-//       const users = querySnapshot.docs.map((doc) => ({
-//         id: doc.id,
-//         ...doc.data(),
-//       }));
-
-import { collection, getDocs, query } from "firebase/firestore";
+import { collection, DocumentData, getDocs, limit, orderBy, query } from "firebase/firestore";
 import { db } from "../../firebase";
 import { appDispatch } from "../store";
 import { setIsLoading, setUsers } from "./reducers";
 import { message } from "antd";
 
-//       console.log("usersss: ", users)
-
-//       return users;
-//     } catch (error: any) {
-//       return rejectWithValue(error.message);
-//     }
-//   }
-// );
 
 export const getUsers = () => async(dispatch: appDispatch) => {
     dispatch(setIsLoading(true))
     try {
         const usersRef = collection(db, "messages");
-        const q = query(usersRef);
-        const querySnapshot = await getDocs(q);
-        const users = querySnapshot.docs.map((doc) => ({
-            id: doc.id,
-            ...doc.data(),
-        }));
+        const messagesSnapshot = await getDocs(usersRef)
+        const lastMessages: { userId: string; message: DocumentData; }[] = []
+
+        for(const messagesDoc of messagesSnapshot.docs){
+            const userId = messagesDoc.id
+            const conversationCollection = collection(db, "messages", userId, "conversation")
+
+            const lastMessageQuery = query(conversationCollection, orderBy("timestamp", "desc"), limit(1))
+
+            const conversationSnapshot = await getDocs(lastMessageQuery)
+
+            conversationSnapshot.forEach((doc) => {
+                lastMessages.push({
+                    userId,
+                    message: doc.data()
+                })
+            })
+        }
     
-        console.log("usersss: ", users)
-        dispatch(setUsers(users))
+        dispatch(setUsers(lastMessages))
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error:any) {
         message.error(error)

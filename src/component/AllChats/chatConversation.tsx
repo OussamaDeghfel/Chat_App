fde89e0e5c5a58ca19b398cb59458b038ad25c0a
OussamaDeghfel <oussama.deghfel@gmail.com>
2024-12-19 14:12:35 +0100

    fix and enh the stling of message box

--- src/component/AllChats/chatConversation.tsx
+++ src/component/AllChats/chatConversation.tsx
@@ -1,4 +1,3 @@
-
 import { BsSendFill, BsThreeDotsVertical } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
 // import { HiMiniLink } from "react-icons/hi2";
@@ -12,72 +11,78 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
 
 const ChatConversation = () => {
+  const conversation = useSelector(
+    (state: RootState) => state.user.conversation
+  );
 
-  const conversation = useSelector((state: RootState) => state.user.conversation)
-
-  const [textByUser, setTextByUser] = useState<string>("")
+  const [textByUser, setTextByUser] = useState<string>("");
 
   const handleSendMessage = async () => {
     await addDoc(collection(db, "messages"), {
       text: textByUser,
       sender: "user",
       timestamp: new Date(),
-    })
-    console.log("text msg: ", textByUser)
+    });
+    console.log("text msg: ", textByUser);
 
-    setTextByUser('')
-  }
-
+    setTextByUser("");
+  };
 
   return (
     <div className="w-full max-h-screen flex flex-col">
-      {conversation.length == 0 ? (<div className="flex w-full h-full border-b-2 border-slate-400 justify-center items-center">
-        <h1 className="font-bold text-3xl text-white">Select a Chat</h1>
-      </div>) : (<div>
-  {/* Navbar (Top Section) */}
-  <div className="flex w-full h-20 bg-slate-800 border-b-2 border-slate-400 justify-center items-center px-2">
-    <div className="flex flex-col w-full h-fit">
-      <h1 className="font-bold text-2xl text-gray-300">User Name</h1>
-      <span className="text-xs text-blue-300">online 2h ago</span>
-    </div>
-    <div className="flex space-x-8">
-      <CiSearch size={25} color="white" className="cursor-pointer" />
-      <IoIosCall size={25} color="white" className="cursor-pointer" />
-      <BsThreeDotsVertical
-        size={25}
-        color="white"
-        className="cursor-pointer"
-      />
-    </div>
-  </div>
+      {conversation.length == 0 ? (
+        <div className="flex w-full h-full border-b-2 border-slate-400 justify-center items-center">
+          <h1 className="font-bold text-3xl text-white">Select a Chat</h1>
+        </div>
+      ) : (
+        <div className="flex flex-col w-full h-full">
+          {/* Navbar (Top Section) */}
+          <div className="flex w-full h-20 bg-slate-800 border-b-2 border-slate-400 justify-center items-center px-2">
+            <div className="flex flex-col w-full h-fit">
+              <h1 className="font-bold text-2xl text-gray-300">User Name</h1>
+              <span className="text-xs text-blue-300">online 2h ago</span>
+            </div>
+            <div className="flex space-x-8">
+              <CiSearch size={25} color="white" className="cursor-pointer" />
+              <IoIosCall size={25} color="white" className="cursor-pointer" />
+              <BsThreeDotsVertical
+                size={25}
+                color="white"
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
 
-  {/* Conversation Area (Middle Section) */}
-  <div className="flex-1 overflow-y-scroll ">
-    <Conversation conv={conversation} />
-  </div>
+          {/* Conversation Area (Middle Section) */}
+          <div className="flex-1 overflow-y-scroll ">
+            <Conversation conv={conversation} />
+          </div>
 
-  {/* Message Input (Bottom Section) */}
-  <div className="flex w-full h-fit p-4 bg-slate-800 justify-center items-center space-x-5 py-2">
-    {/* <div className="flex w-fit h-fit space-x-2">
+          {/* Message Input (Bottom Section) */}
+          <div className="flex w-full h-fit p-4 bg-slate-800 justify-center items-center space-x-5 py-2">
+            {/* <div className="flex w-fit h-fit space-x-2">
       <HiMiniLink size={25} color="white" className="cursor-pointer" />
       <MdInsertPhoto size={25} color="white" className="cursor-pointer" />
     </div> */}
 
-    <div className="w-full flex justify-center items-center space-x-2">
-      <input
-        type="text"
-        placeholder="Your message ..."
-        className="w-full h-10 bg-slate-800 border-2 border-slate-500 rounded-md px-2 outline-none text-white" 
-        onChange={(e) => setTextByUser(e.target.value)}
-      />
-      <BsSendFill size={25} color="white" className="cursor-pointer" onClick={handleSendMessage} />
+            <div className="w-full flex justify-center items-center space-x-2">
+              <input
+                type="text"
+                placeholder="Your message ..."
+                className="w-full h-10 bg-slate-800 border-2 border-slate-500 rounded-md px-2 outline-none text-white"
+                onChange={(e) => setTextByUser(e.target.value)}
+              />
+              <BsSendFill
+                size={25}
+                color="white"
+                className="cursor-pointer"
+                onClick={handleSendMessage}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
-  </div>
-      </div>)}
-      
-      
-</div>
-
   );
 };
 
--- src/component/AllChats/conversation.tsx
+++ src/component/AllChats/conversation.tsx
@@ -3,11 +3,11 @@ const Conversation = ({conv}: any ) => {
 
   return (
     <div className="p-2 justify-end items-end bottom-0 space-y-4 flex-reverse">
-      <div className="flex flex-col justify-end items-start bg-gray">
+      <div className="flex flex-col justify-end items-start bg-gray space-y-2">
 
         {conv.map((msg) => {
           return (
-            <p className="bg-gray-200 p-4 w-fit rounded-md">{msg.text}</p>
+            <p className="bg-gray-200 px-4 py-2 w-fit rounded-md">{msg.text}</p>
           )
         })}
       
